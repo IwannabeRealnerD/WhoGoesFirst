397353501f5e42a86fc537e4b1c71a51c4d76ada
IwannabeRealnerD <IwannabeRealnerD@gmail.com>
2022-01-04 13:21:03 +0900

    hell

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
+  purge: { content: ["./src/**/*.tsx"] },
+  darkMode: false, // or 'media' or 'class'
 
-    theme: {
-        fontFamily: {
-            body: ["font-mono"],
-        },
-        extend: {
-            screens: {
-                sm: "330px",
-                md: "768px",
-                lg: "847px",
-                xl: "1280px",
-                "2xl": "1441px",
-                "hover-hover": { raw: "(hover: hover)" },
-            },
-        },
+  theme: {
+    fontFamily: {
+      body: ["font-mono"],
     },
-    //customize about hover, focus, active ...
-    variants: {
-        display: ["responsive", "group-hover"],
-        extend: { backgroundColor: ["active"], opacity: ["disabled"] },
+    extend: {
+      screens: {
+        sm: "330px",
+        md: "768px",
+        lg: "847px",
+        xl: "1280px",
+        "2xl": "1441px",
+        "hover-hover": { raw: "(hover: hover)" },
+      },
     },
+  },
+  //customize about hover, focus, active ...
+  variants: {
+    display: ["responsive", "group-hover"],
+    extend: { backgroundColor: ["active"], opacity: ["disabled"] },
+  },
 
-    plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms")],
 };
